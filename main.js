d763dea232b049a60ddd348bce1a88d6a3c2c874
Adryann Layon <adryann.layon7@gmail.com>
2022-12-05 20:09:53 -0300

    Feature: inputVerify, reset and toggleScreen

--- main.js
+++ main.js
@@ -1,33 +1,61 @@
+// variáveis
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
-const randomNumber = Math.round(Math.random() * 10)
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keypress', reset)
+
+// Funções
 function handleTryClick(event) {
   event.preventDefault()
   const inputNumber = document.querySelector('#inputNumber')
 
-  if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add('hide')
-    screen2.classList.remove('hide')
+  inputVerify(inputNumber)
 
-    document.querySelector(
-      '.screen2 h2'
-    ).innerText = `Acertou em ${xAttempts} tentativas`
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+    screen2.querySelector('h2').innerText = `Acertou em ${xAttempts} tentativas`
   }
 
   inputNumber.value = ''
   xAttempts++
+  console.log(xAttempts)
 }
 
-// Eventos
-
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
-
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function () {
-  screen1.classList.remove('hide')
-  screen2.classList.add('hide')
+function handleResetClick() {
+  toggleScreen()
   xAttempts = 1
-})
+  randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
+
+function reset(e) {
+  if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
+}
+
+function inputVerify(inputNumber) {
+  if (inputNumber.value === '') {
+    alert('Invalid value')
+    inputNumber.value = ''
+    xAttempts--
+  } else {
+    if (Number(inputNumber.value < 0 || inputNumber.value > 10)) {
+      alert('Please, enter a number between 0 and 10.')
+      inputNumber.value = ''
+      xAttempts--
+    }
+  }
+}
